74f638b5875886f9ea6107650c874958fb880eae
DESKTOP-L8U3P3V\Nasri <narsiyatim@hotmail.com>
2017-03-06 10:28:22 +0200

    Add project files.

---
+++ TextPlagiarismWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TextPlagiarismWebApp.Startup))]
+namespace TextPlagiarismWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
